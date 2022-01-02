9dffe55ea9922d369d7c3727834c22478aee911f
ErezKatsman <erezkatsman2311@gmail.com>
2022-01-02 22:44:20 +0200

    finished get  request "/api/pokemon/:name"

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,9 +1,14 @@
+const getDeatailsByName = require("../utils/pokeAPI");
+
 const { Router } = require("express");
 
 const pokemon = Router();
 
-pokemon.get("/", (req, res) => {
-  res.send("Pokemon route");
+pokemon.get("/:name", async (req, res) => {
+  const { name } = req.params;
+  const data = await getDeatailsByName(name);
+  console.log(data);
+  res.json(data);
 });
 
 module.exports = pokemon;
--- utils/pokeAPI.js
+++ utils/pokeAPI.js
@@ -1,4 +1,14 @@
 const { default: axios } = require("axios");
 const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2";
 
-// You can use this file to write helper functions
+const getDeatailsByName = async (name) => {
+  const data = await axios.get(`${POKEAPI_BASE_URL}/pokemon/${name}`);
+  const { height, weight, id, types } = data.data;
+  const sprites = {
+    back_default: data.data.sprites.back_default,
+    front_default: data.data.sprites.front_default,
+  };
+  return { height, weight, id, sprites, types };
+};
+
+module.exports = getDeatailsByName;
