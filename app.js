e947f7b978255d783940b8933c096bc3e325bb71
ErezKatsman <erezkatsman2311@gmail.com>
2022-01-21 21:20:44 +0200

    Version 1.0.0

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const api = require("./routes");
 const app = express();
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
 
 app.use(express.static("build"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use("/api", api);
 
